158e0eb29692c6f5962e83b20db9579baba06f31
Milos Zarkovic <milos.zarkovic@holywally.com>
2023-08-27 17:14:18 +0200

    add dockerized database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import LinkedInProvider from "next-auth/providers/linkedin";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [
         LinkedInProvider({
             clientId: process.env.LINKEDIN_CLIENT_ID ?? "",
